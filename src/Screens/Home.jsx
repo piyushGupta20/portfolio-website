be18b7ae6ce507dde6d8fe3a7e046a1f7cc931b5
Peeush Gupta <piyushguptanai@gmail.com>
2023-01-28 16:00:55 +0530

    second  commit

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
         <Image width={300} className="m-10"/>
         <div className='m-10'>
         <h1 className='text-orange-400 text-5xl mt-6 '>PEEUSH GUPTA</h1>
-        <h3 className='text-orange-300 text-xl mt-6'>web developer,coding enthuthiast and student</h3>
+        <h3 className='text-orange-300 text-xl mt-6'>web developer,coding enthuthiast and student and git commit</h3>
         <h5 className='text-orange-100 text-sm mt-6'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur impedit libero sapiente. Harum quidem repellendus voluptatum, libero eaque culpa cupiditate!</h5>
     </div>
         </div>
